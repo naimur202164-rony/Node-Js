d8617cea83f1697c86dc33183246702809f96e72
Your Name <naimur202163@gmail.com>
2022-09-03 17:20:28 +0600

    working with local server

--- core_modules/index.js
+++ core_modules/index.js
@@ -1,7 +1,9 @@
 const http = require("http");
 
-const server = http.createServer((req, res) => {
-    console.log(`the server is running on port 5000`)
-});
+// const server = http.createServer((req, res) => {
+//   console.log(`the server is running on port 5000`);
+// });
+
+console.log(http)
 
 server.listen(5000);
