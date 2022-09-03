8dfbf9116f1ff1b228e4a3e3055bbdd981c363f1
Your Name <naimur202163@gmail.com>
2022-09-03 17:37:21 +0600

    server added

--- core_modules/index.js
+++ core_modules/index.js
@@ -6,10 +6,15 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   res.end("Hello Node Js");
-  console.log(`contected to the server`);
+  console.log(req.url);
+  if (req.url == "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<p>This is Home Page </p>");
+    res.end
+  }
 });
 
 const port = 5000;
-console.log('the server is running at 500')
+console.log("the server is running at 500");
 
 server.listen(port);
