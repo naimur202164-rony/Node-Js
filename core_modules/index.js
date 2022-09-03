3f405c2352479b5ba99749b0d440b512f54bc95c
Your Name <naimur202163@gmail.com>
2022-09-03 15:20:43 +0600

    basic server added

--- core_modules/index.js
+++ core_modules/index.js
@@ -1,3 +1,7 @@
-const other =require('./other')
-var name = "Naimur Rahman";
-console.log(other)
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    console.log(`the server is running on port 5000`)
+});
+
+server.listen(5000);
